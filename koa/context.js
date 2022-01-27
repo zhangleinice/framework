b7250a5eb65854ea28025430faeff659dac2475c
rocky <rocky.zhang@ximalaya.com>
2022-01-27 18:40:45 +0800

    feat: ...

---
+++ koa/context.js
@@ -0,0 +1,25 @@
+const delegate = require("delegates");
+
+module.exports = {
+  inspect() {},
+  toJSON() {},
+  throw() {},
+  onerror() {},
+};
+
+const proto = module.exports;
+
+// 当你调用ctx.set()时，实际调用的是ctx.response.set();
+// 可以少写一个response
+delegate(proto, "response")
+  .method("set")
+  .method("append")
+  .access("message")
+  .access("body");
+
+//   通过ctx.accepts()这样来调用到ctx.request.accepts()
+delegate(proto, "request")
+  .method("acceptsLanguages")
+  .method("accepts")
+  .access("querystring")
+  .access("socket");
--- koa/mykoa.js
+++ koa/mykoa.js
@@ -3,6 +3,8 @@
  */
 
 const http = require("http");
+const Emitter = require("events");
+const context = require("./context");
 
 /**
  * 1. 校验
@@ -18,26 +20,48 @@ function compose(middleware_list) {
     }
   }
   return function fn(ctx, next) {
+    return dispatch(0);
     function dispatch(i) {
-      let middleware = middleware_list[i];
+      let fn = middleware_list[i];
+
+      if (i === middleware_list.length) {
+        // 这里让fn等于外部传进来的next，其实是进行收尾工作，比如返回404
+        fn = next;
+      }
+      // 如果外部没有传收尾的next，直接就resolve
+      if (!fn) {
+        return Promise.resolve();
+      }
+      // 中间件里面调用next的时候其实调用的是dispatch(i + 1)，也就是执行下一个中间件
       try {
-        return Promise.resolve(middleware(ctx, dispatch.bind(null, i + 1)));
+        return Promise.resolve(fn(ctx, dispatch.bind(null, i + 1)));
       } catch (error) {
         return Promise.reject(error);
       }
     }
-    return dispatch(0);
   };
 }
 
-class Koa {
+class Application extends Emitter {
   constructor() {
+    super();
     this.middleware_list = [];
+    this.context = context;
+  }
+
+  createContext(req, res) {
+    const context = Object.create(this.context);
+    context.app = this;
+    context.req = req;
+    context.res = res;
+    return context;
   }
 
   callback() {
+    // compose 将中间件合并成一个函数
     const fn = compose(this.middleware_list);
-
+    // callback返回值必须符合http.createServer参数形式
+    // 即 (req, res) => {}
     const handleRequest = (req, res) => {
       const ctx = this.createContext(req, res);
       return this.handleRequest(ctx, fn);
@@ -54,14 +78,25 @@ class Koa {
   }
 
   listen(...args) {
+    // callback是传给http.createServer的回调函数，这个函数必须符合http.createServer的参数形式
+    // function(req, res){}
     const server = http.createServer(this.callback());
     return server.listen(...args);
   }
+
+  handleRequest(ctx, fn) {
+    const onError = (err) => ctx.onError(err);
+    const handleResponse = () => respond(ctx);
+    return fn(ctx).then(handleResponse).catch(onError);
+  }
 }
 
-const app = new Koa();
+const app = new Application();
 
 app.use(async (ctx, next) => {
   console.log(1);
   await next();
+  console.log(2);
 });
+
+app.listen(8000);
